bdc70b7039e0aceae62d425d194b35fd87db4901
achetcu <gn9986@wayne.edu>
2018-12-09 23:14:39 -0500

    updating service and class names

--- public/service/weather-service.js
+++ public/service/weather-service.js
@@ -4,6 +4,10 @@
             const service = this;
             service.weatherData = [];
             service.locationData = [];
+            service.miami = [];
+            service.seattle = [];
+            service.phoenix = [];
+            service.dallas = [];
             service.userdaypref = null;
             service.usertemppref = null;
             service.boardMessages = [
@@ -41,6 +45,12 @@
             service.getWeather = function({lat, lng} = {}){
                let latitude = lat || 42.331429;
                let longitude = lng || -83.045753;
+               service.miamiWeather();
+               service.seattleWeather();
+               service.phoenixWeather();
+               service.dallasWeather();
+               service.nycWeather();
+
                 return $http.get('/proxy/https://api.darksky.net/forecast/33bfadcb23406507fb40ff261ed9828c/'+ latitude + ',' + longitude).then((responseData) => {
                         service.weatherData = responseData.data;
                         console.log("This is the Weather data", responseData.data);
@@ -49,7 +59,65 @@
 
                     });
             }
-            
+
+            service.miamiWeather = function(){
+                let latitude = 25.775084;
+                let longitude =  -80.194702;
+                 return $http.get('/proxy/https://api.darksky.net/forecast/33bfadcb23406507fb40ff261ed9828c/'+ latitude + ',' + longitude).then((responseData) => {
+                         service.miami = responseData.data;
+                         console.log("This is Miami Weather data", responseData.data);
+                         
+                         return service.miami;
+ 
+                     });
+             }
+             service.seattleWeather = function(){
+                let latitude = 47.603229;
+                let longitude = -122.33028;
+                 return $http.get('/proxy/https://api.darksky.net/forecast/33bfadcb23406507fb40ff261ed9828c/'+ latitude + ',' + longitude).then((responseData) => {
+                         service.seattle = responseData.data;
+                         console.log("This is Seattle Weather data", responseData.data);
+                         
+                         return service.seattle;
+ 
+                     });
+             }
+
+             service.phoenixWeather = function(){
+                let latitude = 33.448204;
+                let longitude = -112.072585;
+                 return $http.get('/proxy/https://api.darksky.net/forecast/33bfadcb23406507fb40ff261ed9828c/'+ latitude + ',' + longitude).then((responseData) => {
+                         service.phoenix = responseData.data;
+                         console.log("This is Phoenix Weather data", responseData.data);
+                         
+                         return service.phoenix;
+ 
+                     });
+             }
+             service.dallasWeather = function(){
+                let latitude = 32.777977;
+                let longitude = -96.796215;
+                 return $http.get('/proxy/https://api.darksky.net/forecast/33bfadcb23406507fb40ff261ed9828c/'+ latitude + ',' + longitude).then((responseData) => {
+                         service.dallas = responseData.data;
+                         console.log("This is dallas Weather data", responseData.data);
+                         
+                         return service.dallas;
+ 
+                     });
+             }
+
+             service.nycWeather = function(){
+                let latitude = 40.713054;
+                let longitude = -73.935242;
+                 return $http.get('/proxy/https://api.darksky.net/forecast/33bfadcb23406507fb40ff261ed9828c/'+ latitude + ',' + longitude).then((responseData) => {
+                         service.nyc = responseData.data;
+                         console.log("This is NYC Weather data", responseData.data);
+                         
+                         return service.nyc;
+ 
+                     });
+             }
+             
         });
 
 }
